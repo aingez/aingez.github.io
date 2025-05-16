75b84069e72f126595de17d34a21876880e08ae0
aingez <aingthawan@gmail.com>
2025-05-16 21:49:08 +0700

    refactor & add nav

---
+++ components/navbar.js
@@ -0,0 +1,36 @@
+class MyNav extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    const wrapper = document.createElement('nav');
+    wrapper.innerHTML = `
+      <style>
+        nav {
+          background: whitexs;
+          padding: 0.5rem;
+          display: flex;
+          gap: 1rem;
+          border-bottom: solid;
+          font-weight: 100;
+        }
+        a {
+          color: black;
+          text-decoration: none;
+          font-size: 1.5em;
+          opacity: 50%;
+        }
+        a:hover {
+          transition: 1s;
+          text-decoration: underline;
+          opacity: 100%;
+        }
+      </style>
+      <a href="index.html">Home</a>
+      <a href="experience.html">Exprience</a>
+      <a href="contact.html">Contact</a>
+    `;
+    shadow.appendChild(wrapper);
+  }
+}
+
+customElements.define('my-nav', MyNav);
